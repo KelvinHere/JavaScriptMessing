6bf079eccd9b6b79ddb126700b2977cc087224e7
KelvinHere <kwardeu@gmail.com>
2020-05-16 16:25:01 +0000

    Feat: Remove old tabe before making a new one

--- assets/js/table-manipulation.js
+++ assets/js/table-manipulation.js
@@ -1,16 +1,28 @@
 /*Create table by size of input fields*/
-function createTable(row,col,inputTable) {
-    /*Get rows and cols from html form input elements*/
+function createTable(row,col,inputTable,inputTableWrapper) {
+    //Get rows and cols from html form input elements
     let rows = document.getElementById(row).value;
     let cols = document.getElementById(col).value;
-    let table = document.getElementById(inputTable);
     rows = parseInt(rows);
     cols = parseInt(cols);
+    let oldTable = document.getElementById(inputTable);
+    
+    //Check for existing table and delete it
+    if (oldTable != null) {
+        oldTable.remove();
+    }
 
-    /*Outer loop to create rows*/
+    //Create table in wrapper
+    let table = document.createElement("TABLE");
+    let wrapper = document.getElementById(inputTableWrapper);
+    wrapper.appendChild(table);
+    table.classList.add("table-class");
+    table.setAttribute("id", "table-to-size");
+    
+    //Outer loop to create rows
     for (let r = 0; r < rows; r++) {
         var row = table.insertRow(0);
-        /*inner loop to create cols*/
+        //inner loop to create cols
         for (let c = 0; c < cols; c++) {
             var cell = row.insertCell(c);
             cell.innerHTML = `R${r}:C${c}`;      //cell text
@@ -20,3 +32,6 @@ function createTable(row,col,inputTable) {
         }
     }
 }
+
+//<table class="table-class" id="table-to-size">
+        //</table>
